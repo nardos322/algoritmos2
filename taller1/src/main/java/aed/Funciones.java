a5948cc57ebdf1756811bc632606fd1906ab3258
nardos322 <nahuel.rlz@gmail.com>
2024-03-23 09:54:23 -0300

    fin taller1

--- taller1/src/main/java/aed/Funciones.java
+++ taller1/src/main/java/aed/Funciones.java
@@ -101,15 +101,31 @@ class Funciones {
 
     boolean esPrefijo(String s1, String s2) {
         // COMPLETAR
-        for(int i = 0; i < s1.length(); i++) {
-            
+        if(s2.length() < s1.length()) {
+            return false;
         }
-        return false;
+        for(int i = 0; i < s1.length(); i++) {
+            if(s1.charAt(i) != s2.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     boolean esSufijo(String s1, String s2) {
         // COMPLETAR
-        return false;
+        String s1Invertido = "";
+        String s2Invertido = "";
+        
+        for(int i = s1.length() - 1; i >= 0; i--) {
+            s1Invertido += s1.charAt(i);
+
+        }
+        for(int i = s2.length() - 1; i >= 0; i--) {
+            s2Invertido += s2.charAt(i);
+
+        }
+        return esPrefijo(s1Invertido, s2Invertido);
     }
 
        
