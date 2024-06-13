1e403334cbcedbe2921218aefe655651b9143ae1
nardos322 <nahuel.rlz@gmail.com>
2024-06-13 20:16:01 -0300

    add tp2

--- TP2/estudiantes/src/main/java/aed/Main.java
+++ TP2/estudiantes/src/main/java/aed/Main.java
@@ -20,7 +20,10 @@ public class Main {
     public static void main(String[] args){
      
         Trie n = new Trie();
-
+        TrieST j = new TrieST<>();
+        j.put("hola", 0);
+        j.put("ho", 0);
+        System.out.print(j);
         n.insert("hola",0);
         n.insert("hooz",1);
         n.insert("h", 2);
